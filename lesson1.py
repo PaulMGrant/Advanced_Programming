f7909ba5c0a202c309668597079f1609cc0f9c85
Paul M Grant <paul@paul-grant.co.uk>
2025-09-19 15:40:14 +0100

    to fix

--- lesson1.py
+++ lesson1.py
@@ -47,10 +47,79 @@ def click_buttons():
   btn_quit=Button(window, text="Quit", command=quit_prog)
   btn_quit.place(x=250, y=500)
 
+#procedure to display list boxes
+#def list_box():
+  #create list box
+  #listbox=Listbox(window)
+  #listbox.insert(1, "Lexmoto")
+  #listbox.insert(2, "BMW")
+  #listbox.insert(3, "Indian")
+  #listbox.insert(4, "Royal Enfield")
+  #listbox.insert(5, "Harley Davidson")
+  #listbox.place(x=400, y=50)
+  #btn_list=Button(window, text="Press Me", command=lambda: display_message("Choice:" +listbox.get(listbox.curselection())))
+  #btn_list.place(x=550, y=80)
+
+#Procedure to display Radio buttons
+#def radio_buttons():
+  #create a string variable to store the pressed button
+  #book_name=StringVar()
+  #create the radio buttons
+  #radio1=Radiobutton(window, text="HTML5", variable=book_name, value="HTML in easy steps")
+  #radio2=Radiobutton(window, text="Python", variable=book_name, value="Python for beginners")
+  #radio3=Radiobutton(window, text="Java", variable=book_name, value="Java for android")
+  #radio4=Radiobutton(window, text="SQL", variable=book_name, value="Advanced SQL")
+  #default button needs to be selected
+  #radio1.select()
+  #radio1.place(x=50, y=50)
+  #radio2.place(x=50, y=75)
+  #radio3.place(x=50, y=100)
+  #radio4.place(x=50, y=125)
+  #add a clickable button
+  #btn_list=Button(window, text="Books", command=lambda: display_message("Selection: " + book_name.get()))
+  #btn_list.place(x=150, y=50)
+
+#procedure for check boxes
+def check_boxes():
+  #create intergers for storing the selection
+  var1=IntVar()
+  var2=IntVar()
+  var3=IntVar()
+  var4=IntVar()
+  #create the check boxes
+  item1=Checkbutton(window, text = "apples", variable=var1, onvalue=1, offvalue=0)
+  item2=Checkbutton(window, text = "Pear", variable=var2, onvalue=1, offvalue=0)
+  item3=Checkbutton(window, text = "Pineapple", variable=var3, onvalue=1, offvalue=0)
+  item4=Checkbutton(window, text = "Oranges", variable=var4, onvalue=1, offvalue=0)
+
+#place the check buttons
+item1.place(x=200, y=300)
+item2.place(x=200, y=325)
+item3.place(x=200, y=350)
+item4.place(x=200, y=375)
+
+#add a button
+btn=Button(window, text="Fruit", command=lambda: checked_items(var1, var2, var3, var4))
+btn.place(x=200, y=375)
+
+#checked items procedure for check boxes
+def checked_items(v1, v2, v3, v4)
+  str_msg="you chose: "
+  if v1.get()==1:
+  str_msg=str_msg+"/nApples"
+    if v1.get()==1:
+  str_msg=str_msg+"/nPear"
+    if v1.get()==1:
+  str_msg=str_msg+"/nPineapple"
+    if v1.get()==1:
+  str_msg=str_msg+"/nOranges"
 #Main programme
 display_window("500x600","interface Test")
 display_label(80, 150, "Enter login name")
 #display_message("This is some message")
 click_buttons()
+#list_box()
+#radio_buttons()
+check_boxes()
 #if not using IDLE
 window.mainloop()
