6a3c5875c313dccc7ee3cfac95b3a2e04f502cd6
Paul M Grant <paul@paul-grant.co.uk>
2025-09-21 13:46:23 +0100

    Imports

---
+++ currency_convertor.py
@@ -0,0 +1,11 @@
+#Title: Currency Convertor
+#Author: Paul Grant
+#Date: 22/09/2025
+#Description: Program to convert currencies
+
+#Note: PEP8 Standards
+
+#Libraries
+from tkinter import *
+import tkinter.messagebox as box
+
