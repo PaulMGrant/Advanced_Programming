cf958d3b32dbb2f809c4b1a838698b0b72482753
Paul M Grant <paul@paul-grant.co.uk>
2025-09-26 20:11:16 +0100

    Assignment 1

--- currency_convertor.py
+++ currency_convertor.py
@@ -9,3 +9,15 @@
 from tkinter import *
 import tkinter.messagebox as box
 
+#Instance
+window=Tk()
+
+#procedures and functions will go here
+def display_window(size,name):
+  window.geometry(size)
+  window.title(name)
+
+#Procedure label
+def display_label(xlbl,ylbl,textlbl):
+  label=Label(window,text=textlbl)
+  label.place(x=xlbl, y=ylbl)
