54efbbab706d25785f4bda9c758d3795c14be638
Paul M Grant <paul@paul-grant.co.uk>
2025-10-03 14:59:17 +0100

    V3.0

--- Databases.py
+++ Databases.py
@@ -9,3 +9,14 @@ import sqlite3
 #create a connection to the database
 #create or open a database called example.db
 con = sqlite3.connect("example.db")
+
+#create the cursor
+cur = con.cursor()
+
+cur.execute("CREATE TABLE IF NOT EXISTS people (first_name text, last_name text, age integer)")
+
+#insert a row of data(called a record)
+#cur.execute("INSERT INTO people VALUES('Peter', 'Moore', '21')")
+
+#commit (or save) the changes
+con.commit()
