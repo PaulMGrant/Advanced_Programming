c48c54c075051b44c3d037584d93a7f8725e4386
Paul M Grant <paul@paul-grant.co.uk>
2025-10-03 14:34:12 +0100

    V1.0

---
+++ Databases.py
@@ -0,0 +1,11 @@
+#Title: SQLite database example
+#Author: Paul Grant
+#Date: 03/10/2025
+#Description: Using databases in python
+
+#Libraries
+import sqlite3
+
+#create a connection to the database
+#create or open a database called example.db
+con = sqlite3.connect("example.db")
